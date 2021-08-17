ada417b379e97303037ad0af2b1332530accf7a0
Nadya <nadya.marchenko@idego.io>
2021-08-17 14:54:04 +0200

    fix: change navigate links

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,13 +4,17 @@ import PhotoAlbumIcon from '@material-ui/icons/PhotoAlbum';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { Header, Logo, NavWrapper } from './NavBar.styled';
 import NavItem from './NavItem';
+import { useLocation } from 'react-router';
 
 
 const NavBar = () => {
+    const currentUrl = useLocation();
+    const currentUser = currentUrl.pathname.match(/\d+/g);
+
     const nav = [
-        { label: 'Photos', icon: <PhotoIcon />, id: 1 },
-        { label: 'Albums', icon: <PhotoAlbumIcon />, id: 2 },
-        { label: 'Profile', icon: <AccountCircleIcon />, id: 3 },
+        { label: 'Photos', icon: <PhotoIcon />, id: 1, link: '/profile/1/photos' },
+        { label: 'Albums', icon: <PhotoAlbumIcon />, id: 2, link: `/profile/${currentUser}/albums` },
+        { label: 'Profile', icon: <AccountCircleIcon />, id: 3, link: '/profile/user' },
     ];
 
     return (
@@ -18,8 +22,8 @@ const NavBar = () => {
             <Logo href="/">Photos App</Logo>
             <NavWrapper>
                 <ul>
-                    {nav.map(({ label, icon, id }) =>
-                        <NavItem key={id} label={label} icon={icon}  />
+                    {nav.map(({ label, icon, id, link }) =>
+                        <NavItem key={id} label={label} icon={icon} link={link}  />
                     )}
                 </ul>
             </NavWrapper>
