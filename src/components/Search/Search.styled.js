ccdeb75e9cab94b6870d335050129bb0be3d4826
Nadya <nadya.marchenko@idego.io>
2021-08-18 11:04:38 +0200

    fix: change styles for input backgrounds and album cards

--- src/components/Search/Search.styled.js
+++ src/components/Search/Search.styled.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
+import { colors } from '../../global/colors';
 
 export const SearchRow = styled.form`
     display: flex;
+
+    & input {
+        background-color: ${colors.bg};
+    }
 `;
