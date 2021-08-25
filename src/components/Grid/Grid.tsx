35e5abbd1f1b13111868d2273ecf20afbe3d474f
Nadya Marchanka <nadia.besarab@gmail.com>
2021-08-25 16:01:44 +0300

    fix: remove unnecessary type for children's grid

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,11 +1,11 @@
-import React, { ReactChild, ReactFragment, ReactPortal } from 'react';
+import React from 'react';
 import { GridLayout } from './Grid.styled';
 import { GridProps } from './Grid.d';
 
 const Grid = ({ children, col }: GridProps) => (
   <GridLayout maxColumnCount={col}>
     {React.Children.toArray(children).map(
-      (item: ReactChild | ReactFragment | ReactPortal) => item,
+      (item) => item,
     )}
   </GridLayout>
 );
