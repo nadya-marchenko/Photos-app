95033138708b181088e1de7820d3497118e97f32
Nadya Marchanka <nadia.besarab@gmail.com>
2021-08-25 11:59:12 +0300

    feat: add test for loading component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,8 +3,8 @@ import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
 import { LoaderArea, LoaderText } from './Loader.styled';
 
 const Loader = () => (
-  <LoaderArea>
-    <HourglassEmptyIcon />
+  <LoaderArea data-testid="loading">
+    <HourglassEmptyIcon data-testid="loading-icon" />
     <LoaderText>Loading...</LoaderText>
   </LoaderArea>
 );
